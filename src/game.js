f54ad938f00ae77d56333c3bb7e97a42595382b7
Naman Agrawal <a6.namanagrawal@gmail.com>
2022-02-13 21:41:31 +0530

    Circle wallah obstricle dall diya

--- src/game.js
+++ src/game.js
@@ -1,4 +1,5 @@
 import Ball from "./Ball.js"
+import Circle from "./Circle.js"
 
 var canvas = document.getElementById("gameContainer")
 var canvasSize = Math.min(window.innerHeight, window.innerWidth)
@@ -11,20 +12,23 @@ const gameSettings = {
 	ballSize: 25,
 	worldSize: 1200,
 	gravity: 2000,
-	worldToScreenCoord: (x) => {
-		return ((x - scroll) * canvasSize) / 1200
+	scroll: (x) => {
+		scroll += x
+	},
+	worldToScreenCoord: (x, xMul = 1) => {
+		return ((x - xMul * scroll) * canvasSize) / 1200
 	},
 }
-
+let circle = new Circle(gameSettings)
 let ball = new Ball(gameSettings)
-console.log("Ball created")
 
-const setFrameBuffers = () => {
+const updateFrameBuffers = () => {
 	canvasSize = Math.min(window.innerHeight, window.innerWidth)
 	canvas.style.height = canvasSize + "px"
 	canvas.style.width = canvasSize + "px"
 	ball.updateFrameBuffers()
+	circle.updateFrameBuffers()
 }
 
-window.onresize = setFrameBuffers
-window.onload = setFrameBuffers
+window.onresize = updateFrameBuffers
+window.onload = updateFrameBuffers
